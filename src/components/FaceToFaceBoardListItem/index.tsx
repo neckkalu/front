bd565ec41b328c68d589f7b80e309ffc9bbb1ce6
C:/Program Files/Git/ <ckswls1265@gmail.com>
2023-09-27 18:25:21 +0900

    requset

--- src/components/FaceToFaceBoardListItem/index.tsx
+++ src/components/FaceToFaceBoardListItem/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 //          component         //
-// description: Board List Item 컴포넌트 //
+// description: Board List Item 컴포넌트 //git add .
 export default function FaceToFaceBoardListItem() {
 
   //          render          //
