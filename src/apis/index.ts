5a489e99d12161c86956afce730f4905718494f4
C:/Program Files/Git/ <ckswls1265@gmail.com>
2023-10-05 18:17:20 +0900

    today

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,1 +1,42 @@
-export {}
+import axios from 'axios';
+import { SignInRequestDto, SignUpRequestDto } from "interface/request";
+import SignInResponseDto from "interface/response/SignInResponseDto";
+import SignUpResponseDto from 'interface/response/SignUpResponseDto';
+import ResponseDto from "./response/Response.dto";
+
+const API_DOMAIN = 'http://localhost:3000/';
+
+const SIGN_UP_URL = () => `${API_DOMAIN}/auth/sign-up`;
+const SIGN_IN_URL = () => `${API_DOMAIN}/auth/sign-in`;
+
+export const signUpRequest = async (data: SignUpRequestDto) => {
+  const result = 
+    await axios.post(SIGN_UP_URL(), data)
+    .then((response) => {
+      const responseBody: SignUpResponseDto = response.data;
+      const { code } = responseBody;
+      return code;
+    })
+    .catch((error) => {
+      const responseBody: ResponseDto = error.response.data;
+      const { code } = responseBody;
+      return code;
+    });
+  return result;
+}
+
+
+export const signInRequest = async (data: SignInRequestDto) => {
+  const result = 
+    await axios.post(SIGN_IN_URL(), data)
+    .then((response) => {
+      const responseBody: SignInResponseDto = response.data;
+      return responseBody;
+    })
+    .catch((error) => {
+      const responseBody: ResponseDto = error.response.data;
+      return responseBody;
+    });
+  return result;
+}
+
---
+++ src/interface/request/index.ts
@@ -0,0 +1,8 @@
+import SignInRequestDto from "./SignInRequestDto";
+import SignUpRequestDto from "./SignUpRequestDto";
+
+
+export type {
+  SignInRequestDto,
+  SignUpRequestDto
+}
