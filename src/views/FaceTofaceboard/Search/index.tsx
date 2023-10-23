c35809640539fb8ecafa9503ef627fa86453b35e
C:/Program Files/Git/ <ckswls1265@gmail.com>
2023-10-23 15:49:00 +0900

    오늘 1

---
+++ src/views/FaceTofaceboard/Search/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './style.css';
+
+
+//     component: 게시물 컴포넌트      //
+export default function FTFBoard() {
+
+//     render: 게시물컴포넌트 랜더링      //  
+  return (
+    <div>index</div>
+  )
+}
